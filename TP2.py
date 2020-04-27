2dd6a31ba5d4066f55657b80b1d315ce42d05409
Thierry LARMIER <TLARMIER@groupement.systeme-u.fr>
2020-04-27 18:25:23 +0200

    Commit TP2, modif Date

--- TP2.py
+++ TP2.py
@@ -6,4 +6,4 @@ yyyy=str(now.year)
 hour=str(now.hour)
 mi=str(now.minute)
 ss=str(now.second)
-print "Date :" + mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+print "Date du jour:" + mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
